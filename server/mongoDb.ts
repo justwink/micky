01253d3f44482b0c0e1e372975f231f8267023dc
christian <baozaodexianyu@163.com>
2018-12-18 10:28:07 +0800

    [update]

--- server/mongoDb.ts
+++
@@ -1,6 +0,0 @@
-import { MongoClient } from './MongoClient'
-import * as mongodb from 'mongodb'
-
-const clientPromise = mongodb.MongoClient.connect('mongodb://localhost:27017')
-
-export default new MongoClient(clientPromise, 'docker')
